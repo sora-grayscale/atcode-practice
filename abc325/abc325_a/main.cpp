176078863cb5801f59d3ab86fb1388daa7ad1b38
susui <ihwiphqkyq4gmyc5pv9qzrg5cqwiv@rdpx5qpz54ibm9auubrs9fmpk5mte.auubrs>
2024-08-14 16:55:08 +0900

    ✨[feat] 325 sucsessed

---
+++ abc325/abc325_a/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#define fast std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+
+int main()
+{
+  std::string name;
+  std::cin >> name;
+  std::cout << name << " san" << std::endl;
+}
