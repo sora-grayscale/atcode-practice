a079e8862a248836da585f7a00dccf9c8d5b6dac
susui <ihwiphqkyq4gmyc5pv9qzrg5cqwiv@rdpx5qpz54ibm9auubrs9fmpk5mte.auubrs>
2024-08-14 20:01:36 +0900

    ✨[feat] successed 337

---
+++ abc337/abc337_a/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#define fast std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+
+int main() {
+  int n, w_taka = 0, w_ao = 0;
+
+  std::cin >> n;
+  for (int i = 0; i < n; i++) {
+    int taka, ao;
+    std::cin >> taka >> ao;
+    w_taka += taka;
+    w_ao += ao;
+  }
+  if (w_taka > w_ao) {
+    std::cout << "Takahashi" << std::endl;
+    return 0;
+  }
+  if (w_taka < w_ao) {
+    std::cout << "Aoki" << std::endl;
+    return 0;
+  }
+  std::cout << "Draw" << std::endl;
+  return 0;
+}
