09dd49045f116070333af4243e1a036742768888
susui <ihwiphqkyq4gmyc5pv9qzrg5cqwiv@rdpx5qpz54ibm9auubrs9fmpk5mte.auubrs>
2024-08-14 20:09:27 +0900

    ✨[feat] successed 338

---
+++ abc338/abc338_a/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#define fast std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+
+int main() {
+  std::string str;
+  std::cin >> str;
+  for (int i = 0; str.size() > i; i++) {
+    if (i == 0) {
+      if (str[i] < 'A' || str[i] > 'Z') {
+        std::cout << "No" << std::endl;
+        return 0;
+      }
+    }
+    else if (str[i] < 'a' || str[i] > 'z') {
+      std::cout << "No" << std::endl;
+      return 0;
+    }
+  }
+  std::cout << "Yes" << std::endl;
+  return 0;
+}
