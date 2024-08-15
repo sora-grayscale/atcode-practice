84d8fafe5e3220c7c5a2c54a577c19c9d7abef99
susui <ihwiphqkyq4gmyc5pv9qzrg5cqwiv@rdpx5qpz54ibm9auubrs9fmpk5mte.auubrs>
2024-08-15 09:06:52 +0900

    ✨[feat] successed 363

---
+++ abc363/abc363_a/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#define fast                                                                   \
+  std::ios::sync_with_stdio(0);                                                \
+  std::cin.tie(0);                                                             \
+  std::cout.tie(0);
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+  int n;
+
+  std::cin >> n;
+  std::cout << 100 - (n % 100) << std::endl;
+}
