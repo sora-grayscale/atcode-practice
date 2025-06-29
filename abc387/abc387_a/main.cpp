653706ad9057b6741ef991f64db6ea5348c6ab00
susui <susui@hatsuyoake.com>
2025-06-30 06:36:32 +0900

    ✨[feat] successed 387 a

---
+++ abc387/abc387_a/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#define fast                                                                   \
+  std::ios::sync_with_stdio(0);                                                \
+  std::cin.tie(0);                                                             \
+  std::cout.tie(0);
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+  int a, b;
+
+  std::cin >> a >> b;
+  std::cout << (a + b) * (a + b) << std::endl;
+}
