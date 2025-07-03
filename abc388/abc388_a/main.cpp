3c92263139b5d0d86f050a6474ac31c98c5ed214
susui <susui@hatsuyoake.com>
2025-07-03 12:02:21 +0900

    ✨[feat] successed 388 a

---
+++ abc388/abc388_a/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#define fast                                                                   \
+  std::ios::sync_with_stdio(0);                                                \
+  std::cin.tie(0);                                                             \
+  std::cout.tie(0);
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+  char c;
+
+  std::cin >> c;
+  std::cout << c << "UPC" << std::endl;
+  return (0);
+}
